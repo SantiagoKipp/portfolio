0746e2b640fb7b976b80f6c9b9a70a58c7762c41
SantiagoKipp <ago007.sk@gmail.com>
2025-06-02 15:20:39 -0500

    Changed resume

--- src/components/Home.js
+++ src/components/Home.js
@@ -145,7 +145,7 @@ function Home() {
           {/* Download Resume Button */}
           <div style={{ textAlign: 'center' }}>
             <a
-              href="/SantiagoKippResumeUpdated.pdf"
+              href={`${process.env.PUBLIC_URL}/SantiagoKippResumeUpdated.pdf`}
               download="Santiago-Kipp-Resume.pdf"
               style={{
                 display: 'inline-block',
